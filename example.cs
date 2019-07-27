b342bf55413ab95e64d3ffa7d5932ab9d9b0588c
James Clarke <32040382+revivedbear@users.noreply.github.com>
2019-07-27 21:02:03 +0100

    Created example runtime demo

---
+++ example.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Load the program into a string
+        StringBuilder sb = new StringBuilder();
+        using (StreamReader sr = new StreamReader("F:\\test.pluto"))
+        {
+            String line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                sb.AppendLine(line);
+            }
+        }
+        string program = sb.ToString();
+        
+        // Create a new Pluto runtime and execute the program
+        PlutoRuntime runtime = new PlutoRuntime();
+        runtime.compile(program);
+
+        Console.ReadLine();
+    }
+}
